40aa79340b95babd19f747ef7ee59caeb1b64511
Wilian Da Silva <wsilva@wabtec.com>
2021-09-30 11:26:23 -0300

    30/09 11:26

--- ControleVendas/FrmTelaInicial.vb
+++ ControleVendas/FrmTelaInicial.vb
@@ -407,4 +407,14 @@ Public Class FrmTelaInicial
         form.ShowDialog()
 
     End Sub
+
+    Private Sub CboxRel1_02_Click(sender As Object, e As EventArgs) Handles CboxRel1_02.Click
+        Dim form = New FrmRecebimentos
+        form.ShowDialog()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim form = New FrmRecebimentos
+        form.ShowDialog()
+    End Sub
 End Class
